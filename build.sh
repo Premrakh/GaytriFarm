45b65e38256ed032ece59c5772a01c9ae79e0ed3
prem-rakh <premrakh272@gmail.com>
2026-01-07 22:53:45 +0530

    update railiway config file

--- build.sh
+++ build.sh
@@ -18,8 +18,4 @@ poetry install --no-dev
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-# Run migrations
-echo "Running migrations..."
-python manage.py migrate --noinput
-
 echo "Build completed successfully!"
